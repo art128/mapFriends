e9d758164d3256e55d3a388e9410b36682d73588
art128 <arturo.silvelo@gmail.com>
2014-05-06 04:56:33 +0200

    Login successful

--- mapFriends/urls.py
+++ mapFriends/urls.py
@@ -7,8 +7,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$','mapFriends.views.index', name='index'),
-    url(r'^login/$','mapFriends.views.login', name='login'),
+    url(r'^login/$','mapFriends.views.login_view', name='login'),
     url(r'^register/$','mapFriends.views.register', name='register'),
+    url(r'^logout/$','mapFriends.views.logout_view', name='logout'),
+    url(r'^map/$','mapFriends.views.map', name='map'),
   
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mapFriends/views.py
+++ mapFriends/views.py
@@ -3,13 +3,35 @@ from django.template import RequestContext
 from mapFriends.forms import LoginForm, RegistroForm
 from django.contrib.auth.models import User   
 
+from django.contrib.auth import login, logout, authenticate
+from django.http import HttpResponseRedirect
+
 def index(request):
     return render_to_response('index.html', context_instance = RequestContext(request))
 
-def login(request):
-    login = LoginForm()
-    ctx = {'login' : login}
-    return render_to_response('login.html',ctx, context_instance = RequestContext(request))
+def login_view(request):
+    mensaje = ""
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/')
+    else:
+        if request.method == "POST":
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                usuario = authenticate(username=username, password=password)
+                if usuario is not None and usuario.is_active:
+                    login(request, usuario)
+                    return HttpResponseRedirect('/')
+                else:
+                    mensaje = 'Usuario y/o password incorrecto'
+        form = LoginForm()
+        ctx = {'login' : form, 'mensaje' : mensaje}
+        return render_to_response('login.html',ctx, context_instance = RequestContext(request))
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
 
 def register(request):
     register = RegistroForm()
@@ -29,3 +51,6 @@ def register(request):
             return render_to_response('register.html', ctx, context_instance=RequestContext(request))
     ctx = {'register' : register }
     return render_to_response('register.html', ctx, context_instance = RequestContext(request))
+
+def map(request):
+    return 
