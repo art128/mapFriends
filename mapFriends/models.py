353526cba3820eb370da051ae026135253651be3
art128 <arturo.silvelo@gmail.com>
2014-05-15 04:51:50 +0200

    Extending user models, doesnt work

--- mapFriends/models.py
+++ mapFriends/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Facebook(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    uid = models.CharField(max_length=255, primary_key=True)
     access_token = models.CharField(max_length=255)
-    expire_token = models.CharField(max_length=255)
--- mapFriends/views.py
+++ mapFriends/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response, HttpResponseRedirect
 from django.template import RequestContext
 from mapFriends.facebook import * 
 from mapFriends.forms import *
+from mapFriends.models import *
 
 def home(request):
     return render_to_response('home.html', {}, context_instance=RequestContext(request))
@@ -13,33 +14,37 @@ def login(request):
     ctx = {'login' : form}
     return render_to_response('login.html', ctx, context_instance=RequestContext(request))        
 
+
+
 def register(request):
-    url = get_authorization_url(request)
+    if request.method == "POST":
+        print "POST"
+        register = RegistroForm(request.POST)
+        password = register['password']
+        name = request.session['name']
+        token = request.session['token']
+        try:
+            u = User.objects.get(email=name['name'])
+        except User.DoesNotExist:
+            user = User.objects.create_user(username=name['name'], email=name['email'], password=password)
+            facebook_user = UserProfile(user=user, access_token=token['access_token'][0])
+            facebook_user.save()
+            return render_to_response('home.html', {}, context_instance=RequestContext(request))
+        return render_to_response('register.html', {}, context_instance=RequestContext(request))#ERROR CASEW
+
     if 'code' not in request.GET: #Comprobamos si tenemos acceso a sus datos
+        url = get_authorization_url(request)
+        print "REDIRECT"
         return HttpResponseRedirect(url)
-    else:
-        if request.method == "POST":
-            register = RegistroForm(request.POST)
-            if register.is_valid():
 
-                password = register.cleaned_data['password']
-                password_confirm = register.cleaned_data['password_confirm']
-                token = get_token(request)
-                name = get_user_data(request, token['access_token'][0])
-                form = RegistroForm()
-
-                user = User.objects.create_user(username=name['name'], email=name['email'], password=password)
-                facebook_user = Facebook(user=user, uid=name['id'], access_token=token['access_token'][0], expire_token= token['expire_token'][0])
-
-                facebook_user.save()
-
-                return render_to_response('/', context_instance= RequestContext(request))
-            else:
-                ctx = {'register' : form}
-                return render_to_response('register.html', ctx, context_instance=RequestContext(request))            
-        
-        ctx = {'register' : form}
-        return render_to_response('register.html', ctx, context_instance=RequestContext(request))
+    token = get_token(request)
+    name = get_user_data(request, token['access_token'][0])
+    request.session['token'] = token
+    request.session['name'] = name
+    print "WTF"
+    form = RegistroForm()
+    ctx = {'register' : form}
+    return render_to_response('register.html', ctx, context_instance=RequestContext(request))
 
 def auth(request):
     mensage = ''
